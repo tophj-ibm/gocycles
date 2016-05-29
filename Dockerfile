b7d7f89da60a6e52d484d5c5f378e573382dca05
Christopher Jones <tophj@linux.vnet.ibm.com>
2016-05-28 23:47:27 -0500

    app now works, started api

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,15 @@
+# Usage:
+#
+# # Assemble the full dev environment. This is slow the first time.
+# docker build -t gocycles .
+#
+#
+
 FROM ubuntu:16.04
-RUN apt-get update && apt-get install -y -q python-all python-pip
+RUN apt-get update && apt-get install -y -q python-all python-pip vim
 ADD ./webapp/requirements.txt /tmp/requirements.txt
 RUN pip install -qr /tmp/requirements.txt
 ADD ./webapp /opt/webapp
 WORKDIR /opt/webapp
 EXPOSE 5000
-CMD ["python", "app.py"]
+CMD ["python" , "app.py"]
--- webapp/app.py
+++ webapp/app.py
@@ -1,17 +1,30 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+testString = "currently no one is playing the game :("
 
 @app.route('/')
 def hello():
-    return 'Hello world test'
+	return render_template('index.html')
+
+@app.route('/test/players', methods=['GET'])
+def getplayers():
+	return testString
+
+@app.route('/test/players/<string:newplayer>', methods=['POST'])
+def addPlayer():
+	if (testString == 'currently no one is playing the game :('):
+		testString = newplayer	
+		return testString
+	else:
+		testString = testString+" " + newplayer
+		return testString
 
 if __name__ == '__main__':
-    #Bin to PORT if defined, otherwise default to 5000
-    port = (int(os.environ.get('PORT', 5000))
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
-
-
 
